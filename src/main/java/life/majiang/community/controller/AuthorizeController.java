48aad9e946412b0870e59a90d8bea6b46c35ab68
codedrinker <gwangchunlei@gmail.com>
2019-05-09 21:39:20 +0800

    fix

--- src/main/java/life/majiang/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -56,7 +56,7 @@ public class AuthorizeController {
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
-            user.setAvatarUrl(githubUser.getAvatar_url());
+            user.setAvatarUrl(githubUser.getAvatarUrl());
             userMapper.insert(user);
             response.addCookie(new Cookie("token", token));
             return "redirect:/";
--- src/main/java/life/majiang/community/dto/GithubUser.java
+++ src/main/java/life/majiang/community/dto/GithubUser.java
@@ -10,5 +10,5 @@ public class GithubUser {
     private String name;
     private Long id;
     private String bio;
-    private String avatar_url;
+    private String avatarUrl;
 }
