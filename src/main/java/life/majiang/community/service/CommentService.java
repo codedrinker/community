202125d364917b42d5d5e60e4556e53b7ae58472
codedrinker <gwangchunlei@gmail.com>
2019-06-01 08:19:51 +0800

    事务

--- src/main/java/life/majiang/community/service/CommentService.java
+++ src/main/java/life/majiang/community/service/CommentService.java
@@ -10,6 +10,7 @@ import life.majiang.community.model.Comment;
 import life.majiang.community.model.Question;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by codedrinker on 2019/5/31.
@@ -26,6 +27,7 @@ public class CommentService {
     @Autowired
     private QuestionExtMapper questionExtMapper;
 
+    @Transactional
     public void insert(Comment comment) {
         if (comment.getParentId() == null || comment.getParentId() == 0) {
             throw new CustomizeException(CustomizeErrorCode.TARGET_PARAM_NOT_FOUND);
