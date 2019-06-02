46d483e04b0035d385c341cb14dae7b08a24af3a
codedrinker <gwangchunlei@gmail.com>
2019-06-02 09:28:37 +0800

    实现回复列表功能

--- src/main/java/life/majiang/community/dto/CommentCreateDTO.java
+++ src/main/java/life/majiang/community/dto/CommentCreateDTO.java
@@ -6,7 +6,7 @@ import lombok.Data;
  * Created by codedrinker on 2019/5/30.
  */
 @Data
-public class CommentDTO {
+public class CommentCreateDTO {
     private Long parentId;
     private String content;
     private Integer type;
--- src/main/java/life/majiang/community/dto/CommentDTO.java
+++ src/main/java/life/majiang/community/dto/CommentDTO.java
@@ -1,7 +1,20 @@
 package life.majiang.community.dto;
 
+import life.majiang.community.model.User;
+import lombok.Data;
+
 /**
  * Created by codedrinker on 2019/6/2.
  */
+@Data
 public class CommentDTO {
+    private Long id;
+    private Long parentId;
+    private Integer type;
+    private Long commentator;
+    private Long gmtCreate;
+    private Long gmtModified;
+    private Long likeCount;
+    private String content;
+    private User user;
 }
