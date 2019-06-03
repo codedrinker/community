41198b530b21202ce0124ffd27be8035f90c87fb
codedrinker <gwangchunlei@gmail.com>
2019-06-03 09:12:50 +0800

    实现二级评论功能-页面和接口实现

--- src/main/java/life/majiang/community/dto/ResultDTO.java
+++ src/main/java/life/majiang/community/dto/ResultDTO.java
@@ -8,9 +8,10 @@ import lombok.Data;
  * Created by codedrinker on 2019/5/31.
  */
 @Data
-public class ResultDTO {
+public class ResultDTO<T> {
     private Integer code;
     private String message;
+    private T data;
 
     public static ResultDTO errorOf(Integer code, String message) {
         ResultDTO resultDTO = new ResultDTO();
@@ -31,6 +32,14 @@ public class ResultDTO {
         ResultDTO resultDTO = new ResultDTO();
         resultDTO.setCode(200);
         resultDTO.setMessage("请求成功");
+        return resultDTO;
+    }
+
+    public static <T> ResultDTO okOf(T t) {
+        ResultDTO resultDTO = new ResultDTO();
+        resultDTO.setCode(200);
+        resultDTO.setMessage("请求成功");
+        resultDTO.setData(t);
         return resultDTO;
     }
 }
