9ba450169029aaf279deb8defaea034f3b6a4408
codedrinker <gwangchunlei@gmail.com>
2019-06-04 22:03:19 +0800

    提问页面相关问题展示

--- src/main/java/life/majiang/community/mapper/QuestionExtMapper.java
+++ src/main/java/life/majiang/community/mapper/QuestionExtMapper.java
@@ -2,7 +2,12 @@ package life.majiang.community.mapper;
 
 import life.majiang.community.model.Question;
 
+import java.util.List;
+
 public interface QuestionExtMapper {
     int incView(Question record);
+
     int incCommentCount(Question record);
+
+    List<Question> selectRelated(Question question);
 }
