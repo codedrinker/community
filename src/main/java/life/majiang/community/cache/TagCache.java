f4686d964af56c54822d5eb542db13a856a28061
codedrinker <gwangchunlei@gmail.com>
2019-06-05 13:26:30 +0800

    规范提问的标签库

---
+++ src/main/java/life/majiang/community/cache/TagCache.java
@@ -0,0 +1,53 @@
+package life.majiang.community.cache;
+
+import life.majiang.community.dto.TagDTO;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Created by codedrinker on 2019/6/5.
+ */
+public class TagCache {
+    public static List<TagDTO> get() {
+        List<TagDTO> tagDTOS = new ArrayList<>();
+        TagDTO program = new TagDTO();
+        program.setCategoryName("开发语言");
+        program.setTags(Arrays.asList("javascript", "php", "css", "html", "html5", "java", "node.js", "python", "c++", "c", "golang", "objective-c", "typescript", "shell", "swift", "c#", "sass", "ruby", "bash", "less", "asp.net", "lua", "scala", "coffeescript", "actionscript", "rust", "erlang", "perl"));
+        tagDTOS.add(program);
+
+        TagDTO framework = new TagDTO();
+        framework.setCategoryName("平台框架");
+        framework.setTags(Arrays.asList("laravel", "spring", "express", "django", "flask", "yii", "ruby-on-rails", "tornado", "koa", "struts"));
+        tagDTOS.add(framework);
+
+
+        TagDTO server = new TagDTO();
+        server.setCategoryName("服务器");
+        server.setTags(Arrays.asList("linux", "nginx", "docker", "apache", "ubuntu", "centos", "缓存 tomcat", "负载均衡", "unix", "hadoop", "windows-server"));
+        tagDTOS.add(server);
+
+        TagDTO db = new TagDTO();
+        db.setCategoryName("数据库");
+        db.setTags(Arrays.asList("mysql", "redis", "mongodb", "sql", "oracle", "nosql memcached", "sqlserver", "postgresql", "sqlite"));
+        tagDTOS.add(db);
+
+        TagDTO tool = new TagDTO();
+        tool.setCategoryName("开发工具");
+        tool.setTags(Arrays.asList("git", "github", "visual-studio-code", "vim", "sublime-text", "xcode intellij-idea", "eclipse", "maven", "ide", "svn", "visual-studio", "atom emacs", "textmate", "hg"));
+        tagDTOS.add(tool);
+        return tagDTOS;
+    }
+
+    public static String filterInvalid(String tags) {
+        String[] split = StringUtils.split(tags, ",");
+        List<TagDTO> tagDTOS = get();
+
+        List<String> tagList = tagDTOS.stream().flatMap(tag -> tag.getTags().stream()).collect(Collectors.toList());
+        String invalid = Arrays.stream(split).filter(t -> !tagList.contains(t)).collect(Collectors.joining(","));
+        return invalid;
+    }
+}
