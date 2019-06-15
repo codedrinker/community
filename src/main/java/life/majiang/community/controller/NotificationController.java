0ef90fee3f4d2b0ca28f9397a681213c22177b70
codedrinker <gwangchunlei@gmail.com>
2019-06-16 07:03:45 +0800

    完成回复通知功能

---
+++ src/main/java/life/majiang/community/controller/NotificationController.java
@@ -0,0 +1,40 @@
+package life.majiang.community.controller;
+
+import life.majiang.community.dto.NotificationDTO;
+import life.majiang.community.enums.NotificationTypeEnum;
+import life.majiang.community.model.User;
+import life.majiang.community.service.NotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by codedrinker on 2019/6/14.
+ */
+@Controller
+public class NotificationController {
+
+    @Autowired
+    private NotificationService notificationService;
+
+    @GetMapping("/notification/{id}")
+    public String profile(HttpServletRequest request,
+                          @PathVariable(name = "id") Long id) {
+
+        User user = (User) request.getSession().getAttribute("user");
+        if (user == null) {
+            return "redirect:/";
+        }
+        NotificationDTO notificationDTO = notificationService.read(id, user);
+
+        if (NotificationTypeEnum.REPLY_COMMENT.getType() == notificationDTO.getType()
+                || NotificationTypeEnum.REPLY_QUESTION.getType() == notificationDTO.getType()) {
+            return "redirect:/question/" + notificationDTO.getOuterid();
+        } else {
+            return "redirect:/";
+        }
+    }
+}
--- src/main/java/life/majiang/community/exception/CustomizeErrorCode.java
+++ src/main/java/life/majiang/community/exception/CustomizeErrorCode.java
@@ -12,7 +12,8 @@ public enum CustomizeErrorCode implements ICustomizeErrorCode {
     TYPE_PARAM_WRONG(2005, "评论类型错误或不存在"),
     COMMENT_NOT_FOUND(2006, "回复的评论不存在了，要不要换个试试？"),
     CONTENT_IS_EMPTY(2007, "输入内容不能为空"),
-    ;
+    READ_NOTIFICATION_FAIL(2008, "兄弟你这是读别人的信息呢？"),
+    NOTIFICATION_NOT_FOUND(2009, "消息莫非是不翼而飞了？"),;
 
     @Override
     public String getMessage() {
