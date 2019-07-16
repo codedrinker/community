0d9e434efb88b20de55119d9d4ec1ed387768edc
codedrinker <gwangchunlei@gmail.com>
2019-07-16 13:36:48 +0800

    修复数据库脚本

--- src/main/resources/db/migration/V7__Change_id_column_type.sql
+++ src/main/resources/db/migration/V7__Change_id_column_type.sql
@@ -1,2 +1,2 @@
-alter table question alter column id bigint default not null;
-alter table `user` alter column id bigint default not null;
+alter table question modify id bigint NOT NULL;
+alter table `user` modify id bigint NOT NULL;
