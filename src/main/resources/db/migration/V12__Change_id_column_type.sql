9e3b954e9bb23e8c142137ad2fe0fbe653100ddd
codedrinker <gwangchunlei@gmail.com>
2019-07-16 13:40:19 +0800

    修复数据库脚本

--- src/main/resources/db/migration/V12__Change_id_column_type.sql
+++ src/main/resources/db/migration/V12__Change_id_column_type.sql
@@ -1,2 +1,2 @@
-alter table question alter column id bigint auto_increment not null;
-alter table `user` alter column id bigint auto_increment not null;
+alter table question modify id bigint auto_increment not null;
+alter table `user` modify id bigint auto_increment not null
--- src/main/resources/db/migration/V8__Change_creator_column_type.sql
+++ src/main/resources/db/migration/V8__Change_creator_column_type.sql
@@ -1,2 +1,2 @@
-alter table question alter column creator bigint default not null;
-alter table comment alter column commentator bigint default not null;
+alter table question modify creator bigint not null;
+alter table `comment` modify commentator bigint not null
