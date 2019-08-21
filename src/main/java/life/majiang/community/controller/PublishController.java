0f814de641790f0f021fe4e424cd2f98bc3e578b
codedrinker <gwangchunlei@gmail.com>
2019-08-22 07:38:02 +0800

    修复可以输入空格内容

--- src/main/java/life/majiang/community/controller/PublishController.java
+++ src/main/java/life/majiang/community/controller/PublishController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.annotation.security.DeclareRoles;
 import javax.servlet.http.HttpServletRequest;
 
 /**
@@ -57,15 +58,15 @@ public class PublishController {
         model.addAttribute("tag", tag);
         model.addAttribute("tags", TagCache.get());
 
-        if (title == null || title == "") {
+        if (StringUtils.isBlank(title)) {
             model.addAttribute("error", "标题不能为空");
             return "publish";
         }
-        if (description == null || description == "") {
+        if (StringUtils.isBlank(description)) {
             model.addAttribute("error", "问题补充不能为空");
             return "publish";
         }
-        if (tag == null || tag == "") {
+        if (StringUtils.isBlank(tag)) {
             model.addAttribute("error", "标签不能为空");
             return "publish";
         }
